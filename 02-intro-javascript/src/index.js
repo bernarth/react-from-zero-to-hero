677bc1198fe4424902318200eb3bd87a5edba8e3
Bernarth <alberthbernarth75@gmail.com>
2024-03-28 13:09:45 -0400

    Add intro to javascript

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,18 @@
+// variables y constantes
+
+const nombre = 'Alberth';
+const apellido = 'Condori';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if ( true ) {
+  const nombre = 'Peter';
+  let valorDado = 6;
+
+  console.log(nombre, valorDado);
+}
+
+console.log(valorDado);
